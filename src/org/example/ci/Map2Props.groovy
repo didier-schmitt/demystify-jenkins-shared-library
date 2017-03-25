a6426595feaf984957dd83ddc03ba61cdedcbb09
Didier Schmitt <diedi80@gmail.com>
2017-03-25 11:04:16 +0100

    1st test

---
+++ src/org/example/ci/Map2Props.groovy
@@ -0,0 +1,10 @@
+package org.example.ci
+
+class Map2Props {
+
+    def static convert(def map) {
+        String conversion = ''
+
+        map.each {key, value -> conversion = conversion + "${key}=${value}\n".toString()}
+    }
+}
